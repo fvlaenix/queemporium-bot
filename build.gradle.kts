819a61091935089df8f2e61908d3d8db4f73e7f7
fvlaenix <fvlaenixf@gmail.com>
2024-05-17 10:32:51 +0400

    Add rpc

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+import com.google.protobuf.gradle.id
+
 plugins {
     kotlin("jvm") version "1.9.23"
+    id("com.google.protobuf") version "0.9.4"
 }
 
 group = "com.fvlaenix"
@@ -10,6 +13,15 @@ repositories {
 }
 
 dependencies {
+    implementation("io.grpc:grpc-kotlin-stub:1.4.0")
+    implementation("com.google.protobuf:protobuf-java:3.16.3")
+    implementation("com.google.protobuf:protobuf-kotlin:3.24.4")
+    runtimeOnly("io.grpc:grpc-netty-shaded:1.59.0")
+    implementation("io.grpc:grpc-protobuf:1.59.0")
+    implementation("io.grpc:grpc-stub:1.59.0")
+    compileOnly("org.apache.tomcat:annotations-api:6.0.53")
+    protobuf(files("discord-bots-rpc/duplicate-image-request.proto", "discord-bots-rpc/is-alive.proto", "discord-bots-rpc/image.proto"))
+    
     testImplementation(kotlin("test"))
 }
 
@@ -18,4 +30,29 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(11)
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.24.4"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.59.0"
+        }
+        create("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:1.4.0:jdk8@jar"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                id("grpc")
+                id("grpckt")
+            }
+            it.builtins {
+                create("kotlin")
+            }
+        }
+    }
 }
