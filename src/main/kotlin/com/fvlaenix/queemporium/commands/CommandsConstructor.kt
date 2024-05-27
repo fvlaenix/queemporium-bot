c6858cc0c30783eb0a5b3aa3bc2c2da0bf6125b1
fvlaenix <fvlaenixf@gmail.com>
2024-05-28 00:06:21 +0400

    Fix search of ListenerAdapter

--- src/main/kotlin/com/fvlaenix/queemporium/commands/CommandsConstructor.kt
+++ src/main/kotlin/com/fvlaenix/queemporium/commands/CommandsConstructor.kt
@@ -3,6 +3,7 @@ package com.fvlaenix.queemporium.commands
 import com.fvlaenix.queemporium.configuration.BotConfiguration
 import com.fvlaenix.queemporium.configuration.DatabaseConfiguration
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import kotlin.reflect.javaType
 
 object CommandsConstructor {
   private val PACKAGES_CANDIDATES: List<String> = listOf(
@@ -18,9 +19,10 @@ object CommandsConstructor {
     "SetDuplicateChannelCommand"
   )
   
+  @OptIn(ExperimentalStdlibApi::class)
   fun convert(botConfiguration: BotConfiguration, databaseConfiguration: DatabaseConfiguration): List<ListenerAdapter> {
     val commands = mutableListOf<ListenerAdapter>()
-    val features = STANDARD_FEATURES + botConfiguration.features
+    val features = (STANDARD_FEATURES + botConfiguration.features).filter { it.isNotBlank() }
     features.forEach { featureCommand ->
       val (feature, isAdd) = if (featureCommand.startsWith("no-")) {
         featureCommand.removePrefix("no-") to false
@@ -32,7 +34,11 @@ object CommandsConstructor {
           Class.forName(packageCandidate + feature).kotlin
         } catch (e: ClassNotFoundException) { null }
       } ?: throw Exception("Can't find class $feature. Please make sure you wrote it correctly")
-      check(clazz.java.interfaces.contains(ListenerAdapter::class.java)) { "Class ${clazz.java.name} should inherit interface ${ListenerAdapter::class.qualifiedName}" }
+      var superClass: Class<*>? = clazz.java.superclass
+      while (superClass != ListenerAdapter::class.java) {
+        check(superClass != null) { "Class ${clazz.java.name} should inherit interface ${ListenerAdapter::class.qualifiedName}" }
+        superClass = superClass.superclass
+      }
       if (!isAdd) {
         commands.removeAll { clazz.isInstance(it) }
         return@forEach
@@ -42,13 +48,13 @@ object CommandsConstructor {
       check(constructors.size == 1) { "Class $feature should have only one constructor" }
       val constructor = constructors.single()
       val parameters: List<Any> = constructor.parameters.map { kParameter -> 
-        when (kParameter.type) {
-          DatabaseConfiguration::class -> databaseConfiguration
+        when (kParameter.type.javaType) {
+          DatabaseConfiguration::class.java -> databaseConfiguration
           else -> throw IllegalArgumentException("Unsupported parameter type: ${kParameter.type}")
         }
       }
       
-      val newCommand = constructor.call(parameters) as ListenerAdapter
+      val newCommand = constructor.call(*parameters.toTypedArray()) as ListenerAdapter
       commands.add(newCommand)
     }
     return commands
