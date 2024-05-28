bd2d5e2cb38b1ce2129acf97f9febad0983a6925
fvlaenix <fvlaenixf@gmail.com>
2024-05-28 16:53:24 +0400

    Add local channels

--- src/main/kotlin/com/fvlaenix/queemporium/database/MessageId.kt
+++ src/main/kotlin/com/fvlaenix/queemporium/database/MessageId.kt
@@ -3,4 +3,6 @@ package com.fvlaenix.queemporium.database
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class MessageId(val guildId: String, val channelId: String, val messageId: String)
+data class MessageId(val guildId: String?, val channelId: String, val messageId: String) {
+  fun withImageNumber(imageNumber: Int) = ImageId(guildId, channelId, messageId, imageNumber)
+}
